1bc4f3d5c9daf050d8089485d4f4916ad55a5973
Ignacio <ignabelitzky@gmail.com>
2023-06-29 12:09:55 -0300

    Improvements in the code to simulate the starfield with different velocity

--- starfield-simulation/params.h
+++ starfield-simulation/params.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #define VELOCITY .005f
+
+#define WINDOW_WIDTH 800
+
+#define WINDOW_HEIGHT 400
+
+#define NUM_STARS 100
--- starfield-simulation/starfield.cpp
+++ starfield-simulation/starfield.cpp
@@ -5,42 +5,38 @@
 class Star {
     public:
     sf::Vector2f position;
+    sf::Vector2f velocity;
+    bool mustDelete;
 
     Star(float a, float b) {
         std::random_device rd;
         std::mt19937 gen(rd());
-        std::uniform_real_distribution<float> randomX(0, a);
-        std::uniform_real_distribution<float> randomY(0, b);
-        position.x = randomX(gen);
-        position.y = randomY(gen);
+        std::uniform_real_distribution<float> velDist(50.0f, 100.0f);
+        std::uniform_real_distribution<float> angleDist(0.0f, 2 * 3.141592f);
+
+        position = sf::Vector2f(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0);
+        velocity = sf::Vector2f(velDist(gen) * std::cos(angleDist(gen)), velDist(gen) * std::sin(angleDist(gen)));
+        mustDelete = false;
     }
 
-    void update(float width, float height) {
-        if(position.x > width/2) {
-            position.x += VELOCITY;
-        } else {
-            position.x -= VELOCITY;
-        }
-        if(position.y > height/2) {
-            position.y += VELOCITY;
-        } else {
-            position.y -= VELOCITY;
-        }
+    void update() {
+        position += velocity * 0.01f;
     }
 };
 
 int main() {
-    constexpr int windowWidth = 800;
-    constexpr int windowHeight = 400;
+    constexpr int windowWidth = WINDOW_WIDTH;
+    constexpr int windowHeight = WINDOW_HEIGHT;
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Starfield Simulation");
+    window.setFramerateLimit(60);
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> randSize(0.5f, 1.5f);
+    std::uniform_real_distribution<float> randSize(0.5f, 1.0f);
 
     std::vector<Star> stars;
-    for(int i = 0; i < 200; ++i) {
+    for(int i = 0; i < NUM_STARS; ++i) {
         Star star(windowWidth, windowHeight);
         stars.push_back(star);
     }
@@ -56,7 +52,7 @@ int main() {
         // Update and draw stars
         for(auto it = stars.begin(); it != stars.end();) {
             sf::CircleShape shape(randSize(gen));
-            it->update(windowWidth, windowHeight);
+            it->update();
             shape.setPosition(it->position);
             window.draw(shape);
             if(it->position.x < 0 || it->position.x > windowWidth || it->position.y < 0 || it->position.y > windowHeight) {
