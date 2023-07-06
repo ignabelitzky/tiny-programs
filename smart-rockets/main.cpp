ae901370ef62977137a77ebd52a876f01073ebcc
Ignacio <ignabelitzky@gmail.com>
2023-07-06 11:26:08 -0300

    Comments added

--- smart-rockets/main.cpp
+++ smart-rockets/main.cpp
@@ -4,21 +4,25 @@
 #include "population.h"
 
 // Definition of the extern variables
-int count = 0;
-int reached = 0;
+int count = 0;      // Counter variable for tracking lifespan
+int reached = 0;    // Counter variable for tracking how many rockets reached the target
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(width, height), "Smart Rockets");
     window.setVerticalSyncEnabled(true);
 
     float radius = 25.0f;
+
+    // Creating the target circle shape
     sf::CircleShape target(radius);
     target.setFillColor(sf::Color::Red);
     target.setOrigin(radius, radius);
     target.setPosition(width / 2.0f, radius + 10);
 
+    // Creating the population of rockets
     Population population(200);
 
+    // Setting up the text for displaying reached rocket count
     sf::Text text;
     sf::Font font;
     if(!font.loadFromFile("./arial/arial.ttf")) {
@@ -37,18 +41,25 @@ int main() {
                 window.close();
         }
         window.clear();
+
+        // Run the population, update and render rockets
         population.run(window, target.getPosition());
-        ++count;
+
+        ++count; // Increment the lifespan counter
+
+        // Check if lifespan is reached
         if(count == lifespan) {
-            population.evaluate(target.getPosition());
-            population.selection();
+            population.evaluate(target.getPosition());  // Evaluate the rockets
+            population.selection(); // Perform selection and create new generation
             std::string t = "Reached: " + std::to_string(reached);
             text.setString(t);
-            count = 0;
-            reached = 0;
+            count = 0;      // Reset the lifespan counter
+            reached = 0;    // Reset the reached rocket counter
         }
+
         window.draw(text);
         window.draw(target);
+
         window.display();
     }
     return 0;
