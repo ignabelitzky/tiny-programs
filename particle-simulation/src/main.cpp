7c4fcdf969fe73058ffb938d3d8debf58a82c2ac
Ignacio Belitzky <ignabelitzky@gmail.com>
2024-03-02 03:14:42 -0300

    Small refactorization of the code

--- particle-simulation/src/main.cpp
+++ particle-simulation/src/main.cpp
@@ -1,40 +1,27 @@
-#include <SFML/Graphics.hpp>
 #include <vector>
-#include <random>
 #include <iostream>
+#include <cmath>
+#include <omp.h>
+#include "../include/helper.h"
 
-unsigned int size = 15000;
-std::vector<sf::Vector2f> pos;
-std::vector<sf::Vector2f> vel;
-std::vector<sf::Color> cs;
+sf::Clock ctr;
 
-sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "particles", sf::Style::Fullscreen);
-
-sf::RectangleShape p;
+bool mpressed, rpressed;
 
 sf::Vector2i mpos;
 
-float zoom;
-sf::Clock ctr;
-bool mpressed, rpressed;
-
-float sqrt2(sf::Vector2f dif) {
-    return sqrt((dif.x * dif.x) + (dif.y * dif.y));
-}
-
-sf::Vector2f normalise(sf::Vector2f dif) {
-    float sqroot = sqrt2(dif);
-    return sf::Vector2f(dif.x / sqroot, dif.y / sqroot);
-}
-
-void update() {
-    for(unsigned int i = 0; i < size; i++) {
-        if(mpressed) {
+void update(std::vector<sf::Vector2f> &pos, std::vector<sf::Vector2f> &vel, std::vector<sf::Color> &cs)
+{
+    for (size_t i = 0; i < pos.size(); i++)
+    {
+        if (mpressed)
+        {
             sf::Vector2f dif = pos[i] - sf::Vector2f(mpos);
             sf::Vector2f normalised = normalise(dif);
             vel[i] -= sf::Vector2f(normalised.x / 5, normalised.y / 5);
         }
-        if(rpressed) {
+        if (rpressed)
+        {
             sf::Vector2f dif = pos[i] - sf::Vector2f(mpos);
             sf::Vector2f normalised = normalise(dif);
             vel[i] += sf::Vector2f(normalised.x / 5, normalised.y / 5);
@@ -43,9 +30,12 @@ void update() {
         float curdif = abs(vel[i].x) + abs(vel[i].y) + ((sin(ctr.getElapsedTime().asMilliseconds() / 300) * 1.5) + 1.5);
 
         cs[i].b = 255;
-        if(curdif * 20 > 255) {
+        if (curdif * 20 > 255)
+        {
             cs[i].r = 255;
-        } else {
+        }
+        else
+        {
             cs[i].r = curdif * 20;
         }
         vel[i].x *= 0.99;
@@ -54,33 +44,46 @@ void update() {
     }
 }
 
-int genRandomInt(int min, int max) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> distribution(min, max);
-    return distribution(gen);
-}
+int main()
+{
+    unsigned int size = 150000;
+    std::vector<sf::Vector2f> pos;
+    std::vector<sf::Vector2f> vel;
+    std::vector<sf::Color> cs;
 
+    float zoom;
 
-int main() {
+    sf::RectangleShape p;
+
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "particles", sf::Style::Fullscreen);
+
     window.setMouseCursorVisible(false);
-    for(unsigned int i = 0; i < size; i++) {
-        pos.push_back(sf::Vector2f(genRandomInt(0, window.getSize().x), genRandomInt(0, window.getSize().y)));
+
+    int x = window.getSize().x, y = window.getSize().y;
+    for (unsigned int i = 0; i < size; i++)
+    {
+        pos.push_back(sf::Vector2f(genRandomInt(0, x), genRandomInt(0, y)));
         vel.push_back(sf::Vector2f(0, 0));
         cs.push_back(sf::Color::Blue);
     }
+
     zoom = 1;
-    while(window.isOpen()) {
+    while (window.isOpen())
+    {
         sf::Event event;
-        while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed) {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
             }
-            if(event.type == sf::Event::MouseMoved) {
+            if (event.type == sf::Event::MouseMoved)
+            {
                 mpos = sf::Mouse::getPosition(window);
                 mpos = sf::Vector2i((mpos.x / zoom), (mpos.y / zoom));
             }
-            if(event.type == sf::Event::MouseWheelMoved) {
+            if (event.type == sf::Event::MouseWheelMoved)
+            {
                 zoom += event.mouseWheel.delta * 0.01;
                 mpos = sf::Mouse::getPosition(window);
                 mpos = sf::Vector2i((mpos.x / zoom), (mpos.y / zoom));
@@ -92,10 +95,11 @@ int main() {
         rpressed = sf::Mouse::isButtonPressed(sf::Mouse::Right);
         mpressed = sf::Mouse::isButtonPressed(sf::Mouse::Left);
 
-        update();
+        update(pos, vel, cs);
         p.setSize(sf::Vector2f(zoom * 1, zoom * 1));
         p.setFillColor(sf::Color::Yellow);
-        for(unsigned int i = 0; i < pos.size(); i++) {
+        for (size_t i = 0; i < pos.size(); i++)
+        {
             p.setFillColor(cs[i]);
             p.setPosition(pos[i].x * zoom, pos[i].y * zoom);
             window.draw(p);
