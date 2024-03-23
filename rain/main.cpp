6630f4dd5a4d23dddf03e9728bd5abdf3b811b23
Ignacio Belitzky <ignabelitzky@gmail.com>
2024-03-23 18:53:35 -0300

    Refactor of main.cpp

--- rain/main.cpp
+++ rain/main.cpp
@@ -4,40 +4,47 @@
 #include "drop.h"
 #include "splash.h"
 
-int main() {
+int main()
+{
     sf::RenderWindow window(sf::VideoMode(width, height), "Rain");
     window.setVerticalSyncEnabled(true);
 
     std::vector<Drop> drops;
     std::deque<Splash> splashes;
-    for(size_t i = 0; i < dropCount; ++i) {
+    for (size_t i = 0; i < dropCount; ++i)
+    {
         Drop d;
         drops.push_back(d);
     }
-    while(window.isOpen()) {
+    while (window.isOpen())
+    {
         sf::Event event;
-        while(window.pollEvent(event)) {
-            if(event.type == sf::Event::Closed)
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
                 window.close();
         }
         window.clear(sf::Color(230, 230, 250));
-        for(size_t i = 0; i < drops.size(); ++i) {
-            drops.at(i).fall();
-            drops.at(i).show(window);
-            if(drops.at(i).getPosition().y > height - 0.1f) {
-                Splash s(drops.at(i).getPosition());
-                splashes.push_back(s);
+
+        for (auto &drop : drops)
+        {
+            drop.fall();
+            drop.show(window);
+            if (drop.getPosition().y > height - 0.1f)
+            {
+                splashes.emplace_back(drop.getPosition());
             }
-        }
-        for(size_t i = 0; i < splashes.size(); ++i) {
-            splashes.at(i).update();
-            splashes.at(i).show(window);
         }
 
-        if(splashes.size() > 60) {
-            for(int i = 0; i < 30; ++i) {
-                splashes.pop_front();
-            }
+        for (auto &splash : splashes)
+        {
+            splash.update();
+            splash.show(window);
+        }
+
+        if (splashes.size() > 60)
+        {
+            splashes.erase(splashes.begin(), splashes.begin() + 30);
         }
 
         window.display();
