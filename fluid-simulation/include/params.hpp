23fc6a6de395ff3e5b16dc6d34b68e9afedb6682
Ignacio Belitzky <ignabelitzky@gmail.com>
2024-03-27 03:03:24 -0300

    Minor refactoring of the code

--- fluid-simulation/include/params.hpp
+++ fluid-simulation/include/params.hpp
@@ -1,23 +1,6 @@
 #ifndef PARAMS_HPP
 #define PARAMS_HPP
 
-struct Fluid
-{
-    int size;
-    float dt;
-    float diff;
-    float visc;
-
-    float *s;
-    float *density;
-
-    float *Vx;
-    float *Vy;
-
-    float *Vx0;
-    float *Vy0;
-};
-
-typedef struct Fluid Fluid;
+const int N = 800;
 
 #endif
---
+++ fluid-simulation/include/utils.hpp
@@ -0,0 +1,33 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <cmath>
+#include <cstdlib>
+
+struct Fluid
+{
+    int size;
+    float dt;
+    float diff;
+    float visc;
+
+    float *s;
+    float *density;
+
+    float *Vx;
+    float *Vy;
+
+    float *Vx0;
+    float *Vy0;
+};
+
+typedef struct Fluid Fluid;
+
+int IX(int i, int j, int N);
+Fluid *create_fluid(int size, float dt, float diffusion, float viscosity);
+void free_fluid(Fluid *fluid);
+void add_density(Fluid *fluid, int x, int y, float amount);
+void add_velocity(Fluid *fluid, int x, int y, float amountX, float amountY);
+void step(Fluid *fluid, int N);
+
+#endif
